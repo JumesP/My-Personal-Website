6086f055567e672bf11b66270ad8d79c7f2a8c28
James <118614403+JumesP@users.noreply.github.com>
2023-10-13 22:12:53 +0100

    File Update

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,4 +1,45 @@
 function test() {
-    document.getElementById("demo").innerText = "Hello Queen!";
+    document.getElementById("demo").innerText = "Hello World!";
     
 }
+
+var myPhone = {
+    make : "Apple",
+    model : "IPhone 4",
+    warranty : 12,
+    colour : "White"
+};
+
+
+console.log(myPhone);
+console.log(myPhone.model);
+
+var secondPhone = myPhone;
+secondPhone.warranty = 2; // this will change myPhone aswell bc second is set with =
+console.log(secondPhone)
+
+// object using new
+
+var myNewPhone = new Object();
+myNewPhone.make = "Apple";
+myNewPhone.model = "IPhone X"
+myNewPhone.warranty = 12;
+myNewPhone.colour = "Black"
+
+console.log(myNewPhone)
+
+// object using constructor
+
+function phone(make, model, warranty, colour) {
+    this.make = make;
+    this.model = model;
+    this.warranty = warranty;
+    this.colour = colour;
+    this.extendWarranty = function(x) {
+        this.warranty += x;
+    }
+}
+
+var theNewPhone = new phone("Apple", "IPhone X", 12, "Blue");
+
+console.log(theNewPhone)
