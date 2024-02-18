3fccb6873c86ef21108e73850f9b12563a9bf1ba
James <118614403+JumesP@users.noreply.github.com>
2024-02-18 23:54:43 +0000

    fixes

---
+++ src/index.js
@@ -0,0 +1,9 @@
+function link() {
+    window.open("")
+}
+console.log("hi")
+let arduino = document.getElementById("arduino")
+// arduino.click(function () {
+//     console.log("hi")
+//     window.open("https://github.com/JumesP/Temperature-Display")
+// })
